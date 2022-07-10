8f181ec2b370cd9ba7b01d9068644859d25bd441
Taishi <taishw@uw.edu>
2022-07-10 00:10:11 -0700

    changed site title & author

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -17,9 +17,9 @@
 
 /** @type {MetaConfig} */
 const metaConfig = {
-  title: "Dev Ed",
+  title: "Taishi Walden",
   description: `Ed's Blog`,
-  author: "Ed",
+  author: "Taishi Walden",
   siteUrl: "https://gatsby-starter-apple.netlify.app",
   lang: "en",
   utterances: "sungik-choi/gatsby-starter-apple-comment",
